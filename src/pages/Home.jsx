9c5278936e62b248c4ee03f0f4ce5acdfd4fa4b6
Valentin Deshayes <deshayes.gestion@gmail.com>
2026-04-10 17:23:06 +0000

    Resolve generated image URLs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import StatsSection from "../components/home/StatsSection";
 import TestimonialsSection from "../components/home/TestimonialsSection";
 import CTASection from "../components/home/CTASection";
 
-const HERO_IMAGE = "/__generating__/img_c2fa0a857d22.png";
+const HERO_IMAGE = "https://media.base44.com/images/public/69d9306ce333e4b766debaf1/1953f63fb_generated_a7924120.png";
 
 export default function Home() {
   return (
